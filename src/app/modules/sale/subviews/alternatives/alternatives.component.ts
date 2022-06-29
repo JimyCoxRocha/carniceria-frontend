47692863f9ac464a1628553c2d14e030513e3ab9
JimyCox <terraxted@hotmail.com>
2022-06-29 11:18:04 -0500

    Cotización de compra

--- src/app/modules/sale/subviews/alternatives/alternatives.component.ts
+++ src/app/modules/sale/subviews/alternatives/alternatives.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { toJpeg } from 'html-to-image';
+import { BreakpointsService } from 'src/app/core/services/breakpoints.service';
 
 @Component({
   selector: 'app-alternatives',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlternativesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private bp: BreakpointsService) { }
 
   ngOnInit(): void {
+  }
+
+  downloadImage(){
+    var node:any = document.getElementById('comprobante');
+    let width = node.clientWidth;
+    let height = node.clientHeight;
+
+    toJpeg(node, { quality: 0.95, width: width, height: height })
+    .then(function (dataUrl) {
+      var link = document.createElement('a');
+      link.download = 'carniceria-zamorano-comprobante.jpeg';
+      link.href = dataUrl;
+      link.click(); 
+    });
+  }
+
+  breakpoint(point: string){
+/*     console.log("punto: md", this.bp.breakpoint('md'));
+    console.log("punto: lg", this.bp.breakpoint('lg')); */
+    return this.bp.breakpoint(point)
   }
 
 }
